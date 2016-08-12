573c308568410459983b34175d04de277be76afc
momcheva <nora.momcheva@gmail.com>
2016-08-12 17:04:16 +0300

    Add files via upload

---
+++ After10Years.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class After10Years
+    {
+        static void Main()
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("bg-BG");
+
+            string birthDateText = Console.ReadLine();
+
+            DateTime birthDate = DateTime.Parse(birthDateText);
+
+            DateTime currentDate = DateTime.Now;
+
+            int currentAge = currentDate.Year - birthDate.Year;
+
+        }
+    }
